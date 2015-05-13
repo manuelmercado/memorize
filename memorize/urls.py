53ce55fb01287c6a85437f997636bae61a380959
elektrodx <elektrodx@gmail.com>
2015-05-13 17:11:12 -0400

    estilos para login

--- memorize/urls.py
+++ memorize/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^actividades/id=(\d+)$', 'actividades.views.lista', name='lista'),
     url(r'^singin/', 'user_p.views.login_user', name='login_user'),
-    url(r'^logout_p', 'user_p.views.logout_view', name='logout_view'),
+    url(r'^logout_p/', 'user_p.views.logout_view', name='logout_view'),
+    # url(r'^singin2/', 'user_p.views.login_view', name='login_view'),
 ]
--- user_p/views.py
+++ user_p/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from forms import *
 
 def login_user(request):
     logout(request)
@@ -22,3 +23,12 @@ def login_user(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+#def login_view(request):
+#    form = LoginForm(request.POST or None)
+#    if request.POST and form.is_valid():
+#        user = form.login(request)
+#        if user:
+#            login(request, user)
+#            return HttpResponseRedirect("/")# Redirect to a success page.
+#    return render(request, 'login_2.html', {'login_form': form })
