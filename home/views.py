a609577496fe1d93fe491a731ac923e2b447f672
elektrodx <elektrodx@gmail.com>
2015-05-12 23:31:54 -0400

    login y logout sin css app user_p

--- home/views.py
+++ home/views.py
@@ -5,8 +5,10 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from tarea.models import *
+from django.utils.importlib import import_module
+from django.conf import settings
 
-@login_required(login_url="/admin")
+@login_required(login_url="/singin")
 def home(request):
 	q_tareas=Tareas.objects.filter(state=False)
 	home_template = 'home.html'
--- memorize/urls.py
+++ memorize/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^actividades/id=(\d+)$', 'actividades.views.lista', name='lista'),
+    url(r'^singin/', 'user_p.views.login_user', name='login_user'),
+    url(r'^logout_p', 'user_p.views.logout_view', name='logout_view'),
 ]
