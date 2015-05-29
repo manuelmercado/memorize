1e865f0ed4f965a5b84153a2744d2ed73bd5df65
elektrodx <elektrodx@gmail.com>
2015-05-29 19:11:54 -0400

    arreglo de links en el menu
    se redirecciono los links del menu y se cambio el home a tareas
    pendientes

--- memorize/urls.py
+++ memorize/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^$', 'tarea.views.tarea_sin_atencion', name='tarea_sin_atencion'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^singin/', 'user_p.views.login_user', name='login_user'),
     url(r'^logout_p/', 'user_p.views.logout_view', name='logout_view'),
     url(r'^add_tarea/', 'tarea.views.add_tarea', name='add_tarea'),
+    url(r'^tareas_pendientes', 'home.views.home', name='home'),
     url(r'^actividades/add_actividades/id=(\d+)$', 'actividades.views.add_actividades', name='add_actividades'),
     url(r'^tareas/tarea_sa/', 'tarea.views.tarea_sin_atencion', name='tarea_sin_atencion'),
 
