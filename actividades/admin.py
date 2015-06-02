6bc6ff37805f3aa6d5435ac29af3129949a1ccc7
elektrodx <elektrodx@gmail.com>
2015-06-01 21:09:19 -0400

    tuneo de Admin
    se adiciono algunas funcionalidades  al panel de administración

--- actividades/admin.py
+++ actividades/admin.py
@@ -4,5 +4,7 @@ from .models import Actividades
 
 class ActividadesAdmin(admin.ModelAdmin):
 	list_display = ('fecha', 'tarea', 'Descripcion', 'usuario')
+	list_filter = ('usuario',)
+	search_fields = ['tarea__description', 'Descripcion']
 
 admin.site.register (Actividades, ActividadesAdmin)
--- tarea/admin.py
+++ tarea/admin.py
@@ -5,5 +5,7 @@ from .models import Tareas
 
 class TareasAdmin(admin.ModelAdmin):    
     list_display = ('description', 'date_tarea', 'owner', 'state')
+    list_filter = ('owner', 'state')
+    search_fields = ['description',]
 
 admin.site.register(Tareas, TareasAdmin)
